c659a32202ec518e24d20b967683beac09739161
JojoBaPb <sipo.jmudenda@gmail.com>
2025-08-31 14:09:41 +0200

    checker t3 fix001

--- properties/utils.py
+++ properties/utils.py
@@ -9,6 +9,6 @@ def get_all_properties():
     if properties is None:
         # Query the DB and convert to list of dicts for JSON serialization
         properties = list(Property.objects.all().values("id", "title", "description", "price", "location", "created_at"))
-        cache.set("all_properties", properties, 60 * 60)  # Cache for 1 hour
+        cache.set("all_properties", properties, 3600)  # Cache for 1 hour
     return properties
 
