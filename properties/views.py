6f67a8fcc03fa5685a8dd9c4b33caceab235e347
JojoBaPb <sipo.jmudenda@gmail.com>
2025-08-31 13:56:09 +0200

    t1 checker fix00

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
 # Cache the view for 15 minutes (900 seconds)
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all()
-    return render(request, "properties/property_list.html", {"properties": properties})
+    properties = Property.objects.all().values("id", "title", "description", "price", "location", "created_at")
+    data = list(properties)  # Convert QuerySet to list of dicts
+    return JsonResponse({"data": data})
